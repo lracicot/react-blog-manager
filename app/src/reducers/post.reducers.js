cff5b4fd4cdb9dd5430eef0f19c3411daef0f4a7
Louis Racicot <info@louisracicot.com>
2017-12-04 23:07:28 -0500

    posts reducers

--- app/src/reducers/post.reducers.js
+++ app/src/reducers/post.reducers.js
@@ -1,9 +1,42 @@
 
 
+function updatePostSucess(state, post) {
+  const index = state.getIn(['data', 'posts']).findKey(
+    p => p.get('id') === post.get('id'),
+  );
+
+  if (index !== undefined) {
+    return state.setIn(['data', 'posts', index], post);
+  }
+
+  return state;
+}
+
 export default function AppReducers(state, action) {
-  switch (action.type) {
+  const { type } = action;
+
+  switch (type) {
   case 'CREATE_POST_SUCCESS':
-    return state;
+    return state.setIn(
+      ['data', 'posts'],
+      state.getIn(['data', 'posts']).filter(
+        p => p.get('id') !== action.post.get('id'),
+      ).push(action.post),
+    );
+
+  case 'RETREIVE_POSTS_SUCCESS':
+    return state.setIn(['data', 'posts'], action.posts);
+
+  case 'UPDATE_POST_SUCCESS':
+    return updatePostSucess(state, action.post);
+
+  case 'DELETE_POST_SUCCESS':
+    return state.setIn(
+      ['data', 'posts'],
+      state.getIn(['data', 'posts']).filter(
+        p => p.get('id') !== action.postId,
+      ),
+    );
   default:
     return state;
   }
--- app/test/reducers/post.reducers.spec.js
+++ app/test/reducers/post.reducers.spec.js
@@ -1,7 +1,22 @@
-import { Map, List, fromJS } from 'immutable';
+import { Map, List } from 'immutable';
 import { expect } from 'chai';
 
 import PostReducers from '../../src/reducers/post.reducers';
+
+
+const posts = List.of(Map({
+  id: '1',
+  title: 'Test 1',
+  content: 'Lorem ipsum',
+  slug: 'test-1',
+  published_date: '2017-09-20T01:00:00-0400',
+}), Map({
+  id: '2',
+  title: 'Test 2',
+  content: 'Lorem ipsum dolor sit amet',
+  slug: 'test-2',
+  published_date: '2017-10-20T01:00:00-0400',
+}));
 
 describe('Post reducers', () => {
   it('handles CREATE_POST_SUCCESS by adding a post to an empty list', () => {
@@ -11,77 +26,131 @@ describe('Post reducers', () => {
       }),
     });
 
-    const post = fromJS({
-      id: '1',
-      title: 'Test 1',
-      content: 'Lorem ipsum',
-      slug: 'test-1',
-      published_date: '2017-09-20T01:00:00-0400',
-    });
-
     const action = {
       type: 'CREATE_POST_SUCCESS',
-      post,
+      post: posts.get(0),
     };
     const nextState = PostReducers(initialState, action);
 
-    expect(nextState.getIn('data', 'posts', 0)).to.equal(post);
+    expect(nextState.getIn(['data', 'posts']).get(0)).to.equal(posts.get(0));
   });
 
   it('handles CREATE_POST_SUCCESS by adding a second post to a list', () => {
-    const post1 = fromJS({
-      id: '1',
-      title: 'Test 1',
-      content: 'Lorem ipsum',
-      slug: 'test-1',
-      published_date: '2017-09-20T01:00:00-0400',
-    });
-
-    const post2 = fromJS({
-      id: '2',
-      title: 'Test 2',
-      content: 'Lorem ipsum dolor sit amet',
-      slug: 'test-2',
-      published_date: '2017-10-20T01:00:00-0400',
-    });
-
     const initialState = Map({
       data: Map({
-        posts: List.of(post1),
+        posts: List.of(posts.get(0)),
       }),
     });
 
     const action = {
       type: 'CREATE_POST_SUCCESS',
-      post2,
+      post: posts.get(1),
     };
     const nextState = PostReducers(initialState, action);
 
-    expect(nextState.getIn('data', 'posts', 0)).to.equal(post1);
-    expect(nextState.getIn('data', 'posts', 1)).to.equal(post2);
+    expect(nextState.getIn(['data', 'posts']).get(0)).to.equal(posts.get(0));
+    expect(nextState.getIn(['data', 'posts']).get(1)).to.equal(posts.get(1));
   });
 
   it('handles CREATE_POST_SUCCESS by not adding the same post twice', () => {
-    const post = fromJS({
-      id: '1',
-      title: 'Test 1',
-      content: 'Lorem ipsum',
-      slug: 'test-1',
-      published_date: '2017-09-20T01:00:00-0400',
-    });
-
     const initialState = Map({
       data: Map({
-        posts: List.of(post),
+        posts: List.of(posts.get(0)),
       }),
     });
 
     const action = {
       type: 'CREATE_POST_SUCCESS',
-      post,
+      post: posts.get(0),
     };
     const nextState = PostReducers(initialState, action);
 
     expect(nextState).to.equal(initialState);
+    expect(nextState.getIn(['data', 'posts']).get(0)).to.equal(posts.get(0));
+  });
+
+  it('handles RETREIVE_POSTS_SUCCESS by replacing current list', () => {
+    const initialState = Map({
+      data: Map({
+        posts: List.of(posts.get(0)),
+      }),
+    });
+
+    const action = {
+      type: 'RETREIVE_POSTS_SUCCESS',
+      posts: List.of(posts.get(1)),
+    };
+    const nextState = PostReducers(initialState, action);
+
+    expect(nextState.getIn(['data', 'posts'])).to.equal(List.of(posts.get(1)));
+  });
+
+  it('handles UPDATE_POST_SUCCESS by updating a post', () => {
+    const initialState = Map({
+      data: Map({
+        posts: List.of(posts.get(0)),
+      }),
+    });
+
+    const updatedPost = posts.get(0).set('title', 'Test 1 - UPDATED');
+
+    const action = {
+      type: 'UPDATE_POST_SUCCESS',
+      post: updatedPost,
+    };
+    const nextState = PostReducers(initialState, action);
+
+    expect(nextState.getIn(['data', 'posts', 0])).to.equal(updatedPost);
+  });
+
+  it('handles UPDATE_POST_SUCCESS by updating only the right post', () => {
+    const initialState = Map({
+      data: Map({
+        posts: List.of(posts.get(0), posts.get(1)),
+      }),
+    });
+
+    const updatedPost = posts.get(0).set('title', 'Test 1 - UPDATED');
+
+    const action = {
+      type: 'UPDATE_POST_SUCCESS',
+      post: updatedPost,
+    };
+    const nextState = PostReducers(initialState, action);
+
+    expect(nextState.getIn(['data', 'posts', 0])).to.equal(updatedPost);
+    expect(nextState.getIn(['data', 'posts', 1])).to.equal(posts.get(1));
+  });
+
+  it('handles DELETE_POST_SUCCESS by removing the post', () => {
+    const initialState = Map({
+      data: Map({
+        posts: List.of(posts.get(0)),
+      }),
+    });
+
+    const action = {
+      type: 'DELETE_POST_SUCCESS',
+      postId: posts.get(0).get('id'),
+    };
+    const nextState = PostReducers(initialState, action);
+
+    expect(nextState.getIn(['data', 'posts'])).to.equal(List());
+  });
+
+  it('handles DELETE_POST_SUCCESS by removing one post', () => {
+    const initialState = Map({
+      data: Map({
+        posts: List.of(posts.get(0), posts.get(1)),
+      }),
+    });
+
+    const action = {
+      type: 'DELETE_POST_SUCCESS',
+      postId: posts.get(0).get('id'),
+    };
+    const nextState = PostReducers(initialState, action);
+
+    expect(nextState.getIn(['data', 'posts'])).to.equal(List.of(posts.get(1)));
   });
 });
